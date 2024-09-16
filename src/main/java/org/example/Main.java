6aab3c3c335baee8f2ba8492825fece8f0fec94a
Tais Costa <taiscosta2016@hotmail.com>
2024-09-15 22:02:34 -0300

    projeto para praticar java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        int idade = 25;
+        double salarioMensal = 10500.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
+
+        System.out.println("Idade: " + idade);
+    }
+
+
+}
